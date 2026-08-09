b0a649d059a3cb2ad6bf61d047490e490395c203
aarisdaa <65200373@kmitl.ac.th>
2026-08-09 21:02:40 +0700

    feat: connect FastAPI to PostgreSQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,44 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from app.database import engine
+
 
 app = FastAPI(
     title="FlightHunter API",
     version="1.0.0"
 )
 
+
 @app.get("/")
 def root():
     return {
         "message": "FlightHunter API is running!"
     }
+
+
+@app.get("/health")
+def health_check():
+
+    try:
+
+        with engine.connect() as connection:
+
+            result = connection.execute(
+                text("SELECT 1")
+            )
+
+            result.fetchone()
+
+        return {
+            "status": "ok",
+            "database": "connected"
+        }
+
+    except Exception as e:
+
+        return {
+            "status": "error",
+            "database": "disconnected",
+            "detail": str(e)
+        }
